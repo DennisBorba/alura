016aad058ead4d8025f02521e75ee1986a0f338c
Dennis <dennis.borba@hotmail.com>
2016-09-25 00:22:51 -0300

    Integration test - video 4

--- carreiras/desenvolvedor front-end/Node.js/casadocodigo/test/produtos.js
+++ carreiras/desenvolvedor front-end/Node.js/casadocodigo/test/produtos.js
@@ -1,22 +1,14 @@
-var http = require('http');
-var assert = require('assert');
+var express = require('../config/express')();
+var supertest = require('supertest')(express);
 
-describe('#ProdutosController', function() {
-  it('#Listagem json', function(done) {
-    var config = {
-      hostname: 'localhost',
-      port: 3000,
-      path: '/produtos',
-      headers: {
-        'Accept': 'application/json'
-      }
-    };
-    
-    http.get(config, function(res) {
-      assert.equal(res.statusCode, 200);
-      assert.equal(res.headers['content-type'], 'application/json; charset=utf-8');
-      done();
-    });
+describe('ProdutosController', function() {
+  it('Listagem json', function(done) {
+    supertest.get('/produtos')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
   });
 });
+
 /* for run mocha in windows refer to: http://stackoverflow.com/questions/9722407/how-do-you-install-and-run-mocha-the-node-js-testing-module-getting-mocha-co */
+/* npm install supertest --save-dev */
