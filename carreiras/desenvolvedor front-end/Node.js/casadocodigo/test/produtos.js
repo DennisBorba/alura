caf88f55aa6824b9f2cd43782e76e15a1bbeba2b
Dennis <dennis.borba@hotmail.com>
2016-09-24 23:17:07 -0300

    Integration test - Video 1.0

---
+++ carreiras/desenvolvedor front-end/Node.js/casadocodigo/test/produtos.js
@@ -0,0 +1,6 @@
+describe('#ProdutosController', function() {
+  it('#Listagem json', function() {
+    console.log('teste de verificação de listagem de json');
+  });
+});
+/* for run the mocha in windows refer to: http://stackoverflow.com/questions/9722407/how-do-you-install-and-run-mocha-the-node-js-testing-module-getting-mocha-co */
