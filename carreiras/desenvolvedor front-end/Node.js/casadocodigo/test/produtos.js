a421e35ed0acfcc55b7541c56b95dd2eec47d7b3
Dennis <dennis.borba@hotmail.com>
2016-09-24 23:46:07 -0300

    Integration test - video 2

--- carreiras/desenvolvedor front-end/Node.js/casadocodigo/test/produtos.js
+++ carreiras/desenvolvedor front-end/Node.js/casadocodigo/test/produtos.js
@@ -1,6 +1,27 @@
+var http = require('http');
+    
 describe('#ProdutosController', function() {
-  it('#Listagem json', function() {
-    console.log('teste de verificação de listagem de json');
+  it('#Listagem json', function(done) {
+    var config = {
+      hostname: 'localhost',
+      port: 3000,
+      path: '/produtos',
+      headers: {
+        'Accept': 'application/json'
+      }
+    };
+    
+    http.get(config, function(res) {
+      if(res.statusCode == 200) {
+        console.log('Status OK');
+      }
+
+      if(res.headers['content-type'] == 'application/json; charset=utf-8') {
+        console.log('Content type OK');
+      }
+      
+      done();
+    });
   });
 });
-/* for run the mocha in windows refer to: http://stackoverflow.com/questions/9722407/how-do-you-install-and-run-mocha-the-node-js-testing-module-getting-mocha-co */
+/* for run mocha in windows refer to: http://stackoverflow.com/questions/9722407/how-do-you-install-and-run-mocha-the-node-js-testing-module-getting-mocha-co */
